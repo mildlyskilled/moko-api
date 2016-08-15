3a9e0540fc485497cd183190b1c62cc598206a69
Kwabena Aning <kwabena.aning@worldfirst.com>
2016-08-15 11:27:52 +0100

    Explicitly type 'shutdown' to exit the server

--- src/main/scala/MokoWebService.scala
+++ src/main/scala/MokoWebService.scala
@@ -14,8 +14,11 @@ object MokoWebService extends App
     case ex: Exception => println(s"Could not bind to $host:$port")
   }
 
-  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-  StdIn.readLine() // let it run until user presses return
+  println(s"Server online at http://$host:$port/\nType 'shutdown' to stop...")
+  Iterator.continually(StdIn.readLine).takeWhile(_ != "shutdown").foreach { command =>
+    println(s"$command not recognised")
+  }
+
   bindingFuture
     .flatMap(_.unbind()) // trigger unbinding from the port
     .onComplete(_ => system.terminate()) // and shutdown when done
