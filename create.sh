ad1f0a37b0470d2d157361764c20270155179a51
Kwabena Aning <kwabena.aning@gmail.com>
2018-01-21 12:36:58 +0000

    Create database

--- create.sh
+++ create.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+mysql -h localhost -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "CREATE DATABASE mokocharlie;"
 mysql -h localhost -u${MYSQL_USER} -p${MYSQL_PASSWORD} mokocharlie < ./src/main/resources/data/create.sql
