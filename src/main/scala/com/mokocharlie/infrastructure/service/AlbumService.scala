474b2c1bd7a1abd54ac32ffdea8805768882b9a3
Kwabena Aning <kwabena.aning@gmail.com>
2018-01-22 23:12:26 +0000

    WIP

--- src/main/scala/com/mokocharlie/infrastructure/service/AlbumService.scala
+++ src/main/scala/com/mokocharlie/infrastructure/service/AlbumService.scala
@@ -2,50 +2,26 @@ package com.mokocharlie.infrastructure.service
 
 import akka.actor.ActorSystem
 import com.mokocharlie.domain.MokoModel.Album
-import com.mokocharlie.domain.Page
 import com.mokocharlie.domain.common.MokoCharlieServiceError
+import com.mokocharlie.domain.{MokoModel, Page}
 import com.mokocharlie.domain.common.MokoCharlieServiceError.EmptyResultSet
 import com.mokocharlie.domain.common.ServiceResponse.ServiceResponse
 import com.mokocharlie.infrastructure.repository.{DBAlbumRepository, PhotoRepository}
 
+import scala.concurrent.Future
 import scala.collection.immutable.Seq
 import scala.concurrent.ExecutionContextExecutor
 
-class AlbumService(albumRepo: DBAlbumRepository, photoRepository: PhotoRepository)(
+class AlbumService(albumRepo: DBAlbumRepository, photoService: PhotoService)(
     implicit override val system: ActorSystem)
     extends MokoCharlieService {
 
   implicit val ec: ExecutionContextExecutor = system.dispatcher
 
   def createOrUpdate(album: Album): ServiceResponse[Long] = {
+    val cover = album.cover.map(photoService.createOrUpdate)
+
     dbExecute {
-      val cover = album.cover.map(p ⇒ photoRepository.photoById(p.id)).map {
-        case Right(response) ⇒
-          response.map(_.id).getOrElse {
-            album.cover
-              .map { photo ⇒
-                val what = photoRepository.create(
-                  photo.name,
-                  photo.path,
-                  photo.caption,
-                  photo.createdAt,
-                  photo.updatedAt,
-                  photo.deletedAt,
-                  photo.published,
-                  photo.cloudImage,
-                  photo.ownerId)
-
-                what match {
-                  case Right(rPhoto) ⇒ Some(rPhoto)
-                  case Left(_) ⇒ None
-                }
-              }
-          }
-        case Left(_) ⇒ None
-      }
-
-      println("COVER ID", cover)
-
       albumRepo
         .albumById(album.id) match {
         case Right(_) ⇒ albumRepo.update(album)
