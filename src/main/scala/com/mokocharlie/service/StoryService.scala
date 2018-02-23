ac719dd6b6fb83a1f07100c5f3ed42f4b35a6826
Kwabena Aning <kwabena.aning@gmail.com>
2018-02-23 12:03:14 +0000

    Photo story service tests complete

--- src/main/scala/com/mokocharlie/service/StoryService.scala
+++ src/main/scala/com/mokocharlie/service/StoryService.scala
@@ -7,17 +7,23 @@ import com.mokocharlie.domain.common.MokoCharlieServiceError.EmptyResultSet
 import com.mokocharlie.domain.common.ServiceResponse.ServiceResponse
 import com.mokocharlie.infrastructure.repository.StoryRepository
 
-class StoryService(storyRepository: StoryRepository)(
-    implicit val system: ActorSystem)
+class StoryService(storyRepository: StoryRepository)(implicit val system: ActorSystem)
     extends MokoCharlieService {
 
   def list(page: Int, limit: Int, publishedOnly: Option[Boolean]): ServiceResponse[Page[Story]] =
     dbExecute(storyRepository.list(page, limit, publishedOnly))
 
   def createOrUpdate(story: Story): ServiceResponse[Long] =
-    dbExecute{
-      storyRepository.storyById(story.id).toOption.map {_ ⇒
-        storyRepository.update(story)
-      }.getOrElse(storyRepository.create(story))
+    dbExecute {
+      storyRepository
+        .storyById(story.id)
+        .toOption
+        .map { _ ⇒
+          storyRepository.update(story)
+        }
+        .getOrElse(storyRepository.create(story))
     }
+
+  def storyById(id: Long): ServiceResponse[Story] =
+    dbExecute(storyRepository.storyById(id))
 }
--- src/test/scala/com/mokocharlie/service/StoryServiceTest.scala
+++ src/test/scala/com/mokocharlie/service/StoryServiceTest.scala
@@ -17,18 +17,42 @@ class StoryServiceTest extends AsyncFlatSpec with Matchers with TestDBUtils with
 
   behavior of "Story Service"
 
-
-  "Story Service " should "create new story" in {
-    storyService.createOrUpdate(story1).map{
+  "Story Service " should "Create new story" in {
+    storyService.createOrUpdate(story1).map {
       case Right(id) ⇒ id shouldBe 1
       case Left(error) ⇒ fail(s"A photo story should have been created ${error.msg}")
     }
   }
 
-  it should "retrieve all stories" in {
-    storyService.list(1, 10, Some(true)).map{
+  it should "Retrieve all stories" in {
+    storyService.list(1, 10, Some(true)).map {
       case Right(stories) ⇒ stories should have size 1
       case Left(ex) ⇒ fail(s"It should retrieve stories ${ex.msg}")
     }
   }
+
+  it should "Retrieve story by id" in {
+    storyService.storyById(1).map {
+      case Right(story) ⇒
+        story.id shouldBe 1
+        story.name shouldBe "Some photo story"
+      case Left(error) ⇒ fail(s"A story should have been retrieved ${error.msg}")
+    }
+  }
+
+  it should "Update a story" in {
+    storyService.storyById(1).flatMap {
+      case Right(story) ⇒
+        storyService.createOrUpdate(story.copy(name = "Updated story name")).flatMap {
+          case Right(id) ⇒
+            storyService.storyById(id).map {
+              case Right(updatedStory) ⇒ updatedStory.name shouldBe "Updated story name"
+              case Left(error) ⇒ fail(s"A story should have been retrieved ${error.msg}")
+            }
+          case Left(error) ⇒ fail(s"Story should have been updated ${error.msg}")
+        }
+      case Left(error) ⇒ fail(s"Story should have been retrieved ${error.msg}")
+    }
+  }
+
 }
