66e089b9e8a8e29525b8ec76cdba1b239c498229
Kwabena Aning <kwabena.aning@gmail.com>
2018-06-30 17:01:57 +0100

    WIP for ansible vault

--- terraform/api-server.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  shared_credentials_file = "/Users/kwabena/.aws/credentials"
-  region     = "us-east-1"
-}
-
-resource "aws_ecs_cluster" "mokocharlie" {
-
-  name = "moko-api"
-}
