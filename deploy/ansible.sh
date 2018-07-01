89e52ff447fe0378f066d249d7f5d306e7b88bfd
Kwabena Aning <kwabena.aning@gmail.com>
2018-07-01 15:02:31 +0100

    Using vault properly now

--- deploy/ansible.sh
+++ deploy/ansible.sh
@@ -5,7 +5,8 @@ COMMIT_ID=$2
 APPLICATION=$3
 SERVICECOUNT=${4:-1}
 
-ansible-playbook aws/launch-application.yml \
-    --private-key aws/shared/deploy.pem  \
-    --extra-vars "env=$ENV commit_id=$COMMIT_ID application_name=$APPLICATION service_count=$SERVICECOUNT \
-    --vault-password-file scripts_python/vault_pass.py"
+ansible-playbook --vault-password-file "scripts_python/vault_pass.py" \
+    "aws/launch-application.yml" \
+    --private-key "aws/shared/deploy.pem"  \
+    --extra-vars "env=$ENV commit_id=$COMMIT_ID application_name=$APPLICATION service_count=$SERVICECOUNT" 
+    
