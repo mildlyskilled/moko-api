6727ad3fdab7405bc42cbc106db443482a956963
Kwabena Aning <k.aning@elsevier.com>
2018-08-01 16:44:37 +0100

    Add fake token repo to it test

---
+++ src/it/scala/com/mokocharlie/infrastructure/repository/FakeTokenRepository.scala
@@ -0,0 +1,28 @@
+package com.mokocharlie.infrastructure.repository
+
+import java.sql.Timestamp
+import java.time.{Clock, Instant}
+
+import com.mokocharlie.domain.Token
+import com.mokocharlie.domain.common.ServiceResponse.RepositoryResponse
+import com.mokocharlie.infrastructure.repository.common.JdbcRepository
+import com.mokocharlie.service.TestFixtures
+import com.typesafe.config.Config
+
+class FakeTokenRepository(override val config: Config, override val clock: Clock)
+    extends TokenRepository
+    with JdbcRepository
+    with TestFixtures {
+  val fakeToken =
+    Token(
+      "somestupidhasgofdatathatmakesnosense",
+      "somestupidhasgofdatathatmakesnosense",
+      user1.id,
+      Timestamp.from(Instant.now(clock)))
+
+  override def check(token: String): RepositoryResponse[Token] = Right(fakeToken)
+
+  override def store(token: Token): RepositoryResponse[Token] = Right(fakeToken)
+
+  override def refresh(refresh: String, threshold: Timestamp): RepositoryResponse[Token] = Right(fakeToken)
+}
