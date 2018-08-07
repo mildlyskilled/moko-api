8b6ed91a6010c5e10c7c31662d392a953b8f1c9b
Kwabena Aning <k.aning@elsevier.com>
2018-08-07 11:14:29 +0100

    Better healthchec and also using execution context

--- src/main/scala/com/mokocharlie/infrastructure/repository/common/KeepAlive.scala
+++ src/main/scala/com/mokocharlie/infrastructure/repository/common/KeepAlive.scala
@@ -4,20 +4,26 @@ import akka.stream.Materializer
 import akka.stream.scaladsl.Source
 import com.mokocharlie.service.HealthCheckService
 import com.typesafe.scalalogging.StrictLogging
+
+import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.FiniteDuration
 
-class KeepAlive(hc: HealthCheckService)(implicit val materializer: Materializer)
+class KeepAlive(hc: HealthCheckService)(
+    implicit val materializer: Materializer,
+    implicit val ec: ExecutionContext)
     extends StrictLogging {
 
   def run(delay: FiniteDuration, interval: FiniteDuration): Unit =
     Source
       .tick(delay, interval, ())
-      .map(_ ⇒ hc.healthCheck)
-      .runForeach(res ⇒
-        res.foreach {
+      .map(_ ⇒ hc.healthCheck).log("HEALTHCHECK")
+      .mapAsync(1)(res ⇒
+        res.map {
           case Right(status) ⇒
             val checkStatus = if (status) "success" else "failure"
-            logger.info(s"Connection check resulted in [$checkStatus]")
-          case Left(ex) ⇒ logger.info(s"Connection check failed [${ex.msg}]")
-      })
+            s"Connection check resulted in [$checkStatus]"
+          case Left(ex) ⇒
+            s"Connection check failed [${ex.msg}]"
+      }).log("HEALTHCHECK")
+      .runForeach(s ⇒ logger.info(s))
 }
