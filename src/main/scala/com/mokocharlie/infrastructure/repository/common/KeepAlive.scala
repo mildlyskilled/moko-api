8d53b151fe5f79ea4a385e566b3c22ba0f575f25
Kwabena Aning <k.aning@elsevier.com>
2018-08-07 11:01:36 +0100

    Stupid syntax error fix

--- src/main/scala/com/mokocharlie/infrastructure/repository/common/KeepAlive.scala
+++ src/main/scala/com/mokocharlie/infrastructure/repository/common/KeepAlive.scala
@@ -6,17 +6,18 @@ import com.mokocharlie.service.HealthCheckService
 import com.typesafe.scalalogging.StrictLogging
 import scala.concurrent.duration.FiniteDuration
 
-class KeepAlive(hc: HealthCheckService)(implicit val materializer: Materializer) extends StrictLogging {
+class KeepAlive(hc: HealthCheckService)(implicit val materializer: Materializer)
+    extends StrictLogging {
 
-  def run(delay: FiniteDuration, interval: FiniteDuration): Unit = {
-    Source.tick(delay, interval, ())
+  def run(delay: FiniteDuration, interval: FiniteDuration): Unit =
+    Source
+      .tick(delay, interval, ())
       .map(_ ⇒ hc.healthCheck)
-      .runForeach(res ⇒ res.foreach {
-        case Right(status) ⇒
-          val checkStatus = if (status) "success" else "failure"
-          logger.info(s"Connection check resulted in [$checkStatus]")
-        case Left(ex) ⇒ logger.info(s"Connection check failed [${ex.msg}]")
-
-      }
-  }
+      .runForeach(res ⇒
+        res.foreach {
+          case Right(status) ⇒
+            val checkStatus = if (status) "success" else "failure"
+            logger.info(s"Connection check resulted in [$checkStatus]")
+          case Left(ex) ⇒ logger.info(s"Connection check failed [${ex.msg}]")
+      })
 }
