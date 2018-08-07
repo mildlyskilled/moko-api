4c12bf5e25fd8a536616bde13c6a1ede3b1f0a0f
Kwabena Aning <k.aning@elsevier.com>
2018-08-07 10:56:24 +0100

    Show better things

--- src/main/scala/com/mokocharlie/infrastructure/repository/common/KeepAlive.scala
+++ src/main/scala/com/mokocharlie/infrastructure/repository/common/KeepAlive.scala
@@ -6,12 +6,17 @@ import com.mokocharlie.service.HealthCheckService
 import com.typesafe.scalalogging.StrictLogging
 import scala.concurrent.duration.FiniteDuration
 
-
 class KeepAlive(hc: HealthCheckService)(implicit val materializer: Materializer) extends StrictLogging {
 
   def run(delay: FiniteDuration, interval: FiniteDuration): Unit = {
     Source.tick(delay, interval, ())
       .map(_ ⇒ hc.healthCheck)
-      .runForeach(res ⇒ logger.info(s"$res"))
+      .runForeach(res ⇒ res.foreach {
+        case Right(status) ⇒
+          val checkStatus = if (status) "success" else "failure"
+          logger.info(s"Connection check resulted in [$checkStatus]")
+        case Left(ex) ⇒ logger.info(s"Connection check failed [${ex.msg}]")
+
+      }
   }
 }
